570f176045b6428c7db5e63769f4fe2106760a39
alexi <alexi16.f@gmail.com>
2019-07-21 22:12:25 +0200

    Reverted to old version. Added player hitbox in middle. Removed tag from hand so health is not lost when the hand is hit. Turned camera spin on. Added pause and restart controls.

--- Prototype_1_UnityProject(New)/Assets/Scripts/GameManager.cs
+++ Prototype_1_UnityProject(New)/Assets/Scripts/GameManager.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+    bool isPaused = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+            {
+                Time.timeScale = 1;
+                isPaused = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isPaused = true;
+            }
+        }
     }
 }
